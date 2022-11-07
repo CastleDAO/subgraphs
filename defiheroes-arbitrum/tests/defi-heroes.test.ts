ea242f0c858a88b1e999c54dfa0590104e623975
Rafael Ventura <rafinskipg@gmail.com>
2022-11-07 16:47:17 +0100

    Try to add tests

--- defiheroes-arbitrum/tests/defi-heroes.test.ts
+++ defiheroes-arbitrum/tests/defi-heroes.test.ts
@@ -4,59 +4,74 @@ import {
   test,
   clearStore,
   beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
-import { Approval } from "../generated/DefiHeroes/DefiHeroes"
-import { handleApproval } from "../src/defi-heroes"
-import { createApprovalEvent } from "./defi-heroes-utils"
+  afterAll,
+  createMockedFunction,
+} from "matchstick-as/assembly/index";
+import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import { Heroes } from "../generated/schema";
+import { Approval } from "../generated/DefiHeroes/DefiHeroes";
+import { handleApproval, handleTransfer } from "../src/defi-heroes";
+import { createApprovalEvent, createTransferEvent } from "./defi-heroes-utils";
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
-describe("Describe entity assertions", () => {
+const contractAddress = Address.fromString(
+  "0x8ec75bc963181489d7fc1d892f687b8b0987d9ec"
+);
+
+describe("Defi heroes", () => {
   beforeAll(() => {
-    let owner = Address.fromString("0x0000000000000000000000000000000000000001")
-    let approved = Address.fromString(
+    let owner = Address.fromString(
       "0x0000000000000000000000000000000000000001"
-    )
-    let tokenId = BigInt.fromI32(234)
-    let newApprovalEvent = createApprovalEvent(owner, approved, tokenId)
-    handleApproval(newApprovalEvent)
-  })
+    );
+    let to = Address.fromString("0x0000000000000000000000000000000000000002");
+    let tokenId = BigInt.fromI32(234);
+    let newTransferEvent = createTransferEvent(owner, to, tokenId);
+    handleTransfer(newTransferEvent);
+  });
 
   afterAll(() => {
-    clearStore()
-  })
+    clearStore();
+  });
 
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+  test("Heroes created and stored", () => {
+    assert.entityCount("Heroes", 1);
+    let bigIntParam = BigInt.fromString("1");
+
+    // Mock functions
+    createMockedFunction(
+      contractAddress,
+      "tokenURI",
+      "tokenURI(uint256):(string)"
+    )
+      .withArgs([ethereum.Value.fromSignedBigInt(bigIntParam)])
+      .returns([ethereum.Value.fromString("Test")]);
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "ExampleEntity",
+      "Heroes",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "owner",
       "0x0000000000000000000000000000000000000001"
-    )
+    );
     assert.fieldEquals(
-      "ExampleEntity",
+      "Heroes",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "approved",
       "0x0000000000000000000000000000000000000001"
-    )
+    );
     assert.fieldEquals(
-      "ExampleEntity",
+      "Heroes",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
       "tokenId",
       "234"
-    )
+    );
 
     // More assert options:
     // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+  });
+});
