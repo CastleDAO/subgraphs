1d09b88a6eaa1deafedbc7024d85336a18c8c8f9
davidbarbi3r <davidbarbi3r@gmail.com>
2022-11-25 12:01:53 +0100

    :bug: trying to fix bad testing

--- defiheroes-arbitrum/tests/defi-heroes.test.ts
+++ defiheroes-arbitrum/tests/defi-heroes.test.ts
@@ -6,7 +6,7 @@ import {
   beforeAll,
   afterAll,
   beforeEach,
-  logStore
+  logStore,
 } from "matchstick-as/assembly/index";
 import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { Heroe, Quest } from "../generated/schema";
@@ -16,6 +16,7 @@ import {
   handleExperienceGained,
   handleLeveledUp,
   handleQuest,
+  handleTransfer,
 } from "../src/defi-heroes";
 import {
   createExperienceGainedEvent,
@@ -29,7 +30,7 @@ import {
 
 describe("Handle Experience Gained", () => {
   beforeAll(() => {
-    let hero = new Heroe("1");
+    let hero = new Heroe("Joe");
 
     hero.tokenID = BigInt.fromI32(1);
     hero.owner = "0x0";
@@ -56,11 +57,9 @@ describe("Handle Experience Gained", () => {
       BigInt.fromI32(100)
     );
 
-    
-    
-    // logStore()
-    assert.fieldEquals("Heroe", "1", "experience", "10");
+    logStore()
     handleExperienceGained(newExperienceGainedEvent);
+    assert.fieldEquals("Heroe", "1", "speed", "5")
     assert.fieldEquals("Heroe", "1", "experience", "30");
   });
 });
@@ -94,12 +93,10 @@ describe("Hero level up", () => {
       BigInt.fromI32(2)
     );
 
-    logStore()
-    handleLeveledUp(newLevelUp);
-    logStore()
-    
-    
+    // logStore()
+
     assert.fieldEquals("Heroe", "1", "level", "1");
+    handleLeveledUp(newLevelUp);
     assert.fieldEquals("Heroe", "1", "level", "2");
   });
 });
@@ -109,30 +106,40 @@ describe("Handle Quest", () => {
     let quest = new Quest("1");
 
     quest.tokenID = BigInt.fromI32(1);
-    quest.date = BigInt.fromI32(1)
-    quest.xpEarned = BigInt.fromI32(20)
+    quest.date = BigInt.fromI32(1);
+    quest.xpEarned = BigInt.fromI32(20);
 
     quest.save();
   });
 
   test("Create Quest", () => {
     let newQuest = createQuestEvent(
-      BigInt.fromI32(1),
-      BigInt.fromI32(10),
-      BigInt.fromI32(20)
+      BigInt.fromI32(2), //token id
+      BigInt.fromI32(10), //xp gained
+      BigInt.fromI32(20) //xp total
     );
 
-    
     handleQuest(newQuest);
-    
-    // logStore()
-    assert.fieldEquals("Quest", "1", "xpEarned", "0");
-    assert.fieldEquals("Quest", "1", "xpEarned", "20");
+
+    logStore()
+    assert.fieldEquals("Quest", "2", "xpEarned", "0");
+    assert.fieldEquals("Quest", "2", "xpEarned", "20");
   });
 });
 
 describe("Handle transfer", () => {
-  
+  test("Hero creation", () => {
+    let newHeroTransfer = createTransferEvent(
+      Address.fromString("0x0000000000000000000000000000000000000000"),
+      Address.fromString("0x44E8DEd5ba74356a42fE4FA6C698499309a08589"),
+      BigInt.fromI32(10)
+    );
+    
+    assert.notInStore("Heroe", "10");
+    handleTransfer(newHeroTransfer);
+    assert.fieldEquals("Heroe", "10", "id", "10")
+    
+  });
 
   test("Update Hero experience", () => {
     let newExperienceGainedEvent = createExperienceGainedEvent(
@@ -141,12 +148,8 @@ describe("Handle transfer", () => {
       BigInt.fromI32(100)
     );
 
-    
-    
-    // logStore()
     assert.fieldEquals("Heroe", "1", "experience", "10");
     handleExperienceGained(newExperienceGainedEvent);
     assert.fieldEquals("Heroe", "1", "experience", "30");
   });
 });
-
